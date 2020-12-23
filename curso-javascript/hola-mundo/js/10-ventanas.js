db4287595eb4b9444e414379efaf6756e3d5e6f5
kllanquelkevin011197 <k.llanquel@gmail.com>
2020-12-23 16:38:12 -0300

    Tipos de ventanas archivo ventanas.js

---
+++ curso-javascript/hola-mundo/js/10-ventanas.js
@@ -0,0 +1,17 @@
+'use strict'
+
+// ALERTA
+/*
+alert("Esta es mi alerta!!");
+alert("Este es mi texto");
+*/
+
+// CONFIRMACIÓN
+/*
+var resultado = confirm("¿Estas seguro de querer continuar?");
+console.log(resultado);
+*/
+
+// INGRESO DE DATOS
+var mi_resultado = prompt("¿Que edad tienes?", 18);
+console.log(typeof parseInt(mi_resultado));
