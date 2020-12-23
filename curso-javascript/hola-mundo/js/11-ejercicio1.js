27b2cec3b0b75072b60abbaf157809e7f0047e30
kllanquelkevin011197 <k.llanquel@gmail.com>
2020-12-23 17:32:22 -0300

    Plus ejercicio 11

--- curso-javascript/hola-mundo/js/11-ejercicio1.js
+++ curso-javascript/hola-mundo/js/11-ejercicio1.js
@@ -2,16 +2,24 @@
 
 /**
  * Programa que pida 2 numeros y que nos diga cual es el mayor, el menor y si son iguales
+ * Si los numeros no son un numero o son menores o iguales a 0
  */
 
 var numero1 = parseInt(prompt("Ingresa primer numero", 0));
 var numero2 = parseInt(prompt("Ingresa segundo numero", 0));
 
 
+while(numero1 <= 0 || numero2 <= 0 || isNaN(numero1) || isNaN(numero2)){
+    numero1 = parseInt(prompt("Ingresa primer numero", 0));
+    numero2 = parseInt(prompt("Ingresa segundo numero", 0));
+}
+
 if (numero1>numero2) {
     console.log('Numero: ' + numero1 + ' es mayor que nuemro: ' +numero2);
 }else if (numero1 == numero2) {
-    console.log('Los numeros son iguale');
-}else{
+    console.log('Los numeros son iguales');
+}else if (numero1<numero2) {
     console.log('Numero: ' + numero2 + ' es mayor que nuemro: ' +numero1);
+}else{
+    console.log('Numeros con formato incorrecto');
 }
