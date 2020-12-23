ee780c70a711e8adfc3d2fbc7cfce02faddc87df
kllanquel@redvoiss.net <kllanquel@redvoiss.net>
2020-12-23 14:18:01 -0300

    Se registro operacion con bucle for numerosde 1 a 100

--- curso-javascript/hola-mundo/js/08-bucle-for.js
+++ curso-javascript/hola-mundo/js/08-bucle-for.js
@@ -2,3 +2,9 @@
 
 // Bucle FOR
 // Bucle es una estructura de control que se repite varias veces
+
+var numeros = 100;
+
+for (var i = 0; var i < numeros; var i++) {
+	 console.log("Vamos por el numero: "+ i);
+}
